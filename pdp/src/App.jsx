2afd448047f8c9e89a0f093248f58ed46d391764
Venkata Narayanan <venkat.mohan@Venkatas-MacBook-Pro.local>
2024-01-01 10:28:23 +0800

    Added Cart

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./index.scss";
 import Header from "home/Header";
 import Footer from "home/Footer";
+import PDPContent from "./PDPContent";
 
 const App = () => (
-  <div className="text-3xl mx-auto max-w-6xl">
-    <Header/>
-      <div className="my-10">PDP Page Content</div>
-    <Footer/>
-  </div>
+  <Router>
+    <div className="text-3xl mx-auto max-w-6xl">
+      <Header />
+      <Routes>
+        <Route path="/product/:id" Component={PDPContent} />
+      </Routes>
+      <Footer />
+    </div>
+  </Router>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
---
+++ pdp/src/PDPContent.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { getProductById, curreny } from "../../home/src/products";
+
+export default function PDPContent() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      getProductById(id).then(setProduct);
+    } else {
+      setProduct(null);
+    }
+  }, [id]);
+
+  if (!product) return null;
+  return (
+    <div className="grid grid-cols-2 gap-5">
+      <div>
+        <img src={product.image} alt={product.name} />
+      </div>
+      <div>
+        <div className="flex">
+          <h1 className="font-bold text-3xl flex-grow">{product.name}</h1>
+          <div className="font-bold text-3xl flex-end">
+            {curreny.format(product.price)}
+          </div>
+        </div>
+        <div className="mt-10">{product.description}</div>
+        <div className="mt-10">{product.longDescription}</div>
+      </div>
+    </div>
+  );
+}
